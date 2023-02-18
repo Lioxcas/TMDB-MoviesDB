f7256977710f9f89a762b3ac5a3977da592229f6
Lioxcas <licastrox@gmail.com>
2023-02-17 22:41:12 -0300

    'Models'

---
+++ api/models/index.js
@@ -0,0 +1,7 @@
+const User = require("../models/User");
+/* const Favorite = require("../models/Favorite"); */
+
+/* User.belongsToMany(Favorite, { through: "UserFavorites" });
+Favorite.belongsToMany(User, { through: "UserFavorites" }); */
+
+module.exports = { User };
