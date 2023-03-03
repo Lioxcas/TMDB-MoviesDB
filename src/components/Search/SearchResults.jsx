251d302da45ea81ce1416c2483683587524eb816
Lioxcas <licastrox@gmail.com>
2023-03-03 00:07:10 -0300

    Fix  filter results

--- src/components/Search/SearchResults.jsx
+++ src/components/Search/SearchResults.jsx
@@ -7,14 +7,17 @@ const SearchResults = ({ data }) => {
   return (
     <div className="grid grid-cols-4 mx-auto mt-36 gap-4">
       {data &&
-        data.map(({ poster_path, id }) => {
-          return (
-            <Link to={`/${id}`}>
-              <img
-                src={`https://www.themoviedb.org/t/p/w220_and_h330_face${poster_path}`}
-              />
-            </Link>
-          );
+        data.map(({ poster_path, id, media_type }) => {
+          {
+            if (media_type !== "person")
+              return (
+                <Link to={`/${id}`}>
+                  <img
+                    src={`https://www.themoviedb.org/t/p/w220_and_h330_face${poster_path}`}
+                  />
+                </Link>
+              );
+          }
         })}
     </div>
   );
