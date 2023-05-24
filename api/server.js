d40aaab6184ec4776d7e43e960e4a304ddc28251
Lioxcas <licastrox@gmail.com>
2023-05-24 12:49:18 -0300

    Vite and server config

--- api/server.js
+++ api/server.js
@@ -10,23 +10,28 @@ const db = require("../api/db");
 
 const app = express();
 app.use(express.json());
-console.log("server");
+
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    credentials: true,
     origin: "http://localhost:5173",
+    allowedHeaders: ["Content-Type", "SameSite", "withCredentials"],
+    credentials: true,
   })
 );
 app.use(volleyball);
 app.use(morgan("tiny"));
 app.use("/api", routes);
 
-db.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("Escuchando en el puerto ", process.env.PORT);
+db.sync({ force: false })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Escuchando en el puerto ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Error al sincronizar el modelo:", error);
   });
-});
