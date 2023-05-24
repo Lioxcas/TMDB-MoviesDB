5761a67ec8888e0b2e2b4966e080c63f775376a9
Lioxcas <licastrox@gmail.com>
2023-05-24 11:56:59 -0300

    Hooks/userContext

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -1,17 +1,28 @@
 import useAuth from "./useAuth";
 import axios from "axios";
+import useUser from "./useUser";
 
 const useRefreshToken = () => {
   const { setAuth } = useAuth();
+  const { setUser } = useUser();
   const refresh = async () => {
     const response = await axios.get("http://localhost:3001/api/auth/refresh", {
+      headers: { "Content-Type": "application/json" },
       withCredentials: true,
     });
     setAuth((prev) => {
       return {
         ...prev,
         username: response.data.username,
+        favorites: response.data.favorites,
         accessToken: response.data.accessToken,
+      };
+    });
+    setUser((prev) => {
+      return {
+        ...prev,
+        username: response.data.username,
+        favorites: response.data.favorites,
       };
     });
     return response.data.accessToken;
