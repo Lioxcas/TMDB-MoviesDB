2f880fbb7f651d679e492528db3eed85430a4e42
Lioxcas <licastrox@gmail.com>
2023-05-24 11:52:08 -0300

    Server Config

--- api/server.js
+++ api/server.js
@@ -17,16 +17,21 @@ app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    credentials: true,
     origin: "http://localhost:5173",
+    allowedHeaders: ["Content-Type", "SameSite", "withCredentials"],
+    credentials: true,
   })
 );
 app.use(volleyball);
 app.use(morgan("tiny"));
 app.use("/api", routes);
 
-db.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("Escuchando en el puerto ", process.env.PORT);
+db.sync({ force: false })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Escuchando en el puerto ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Error al sincronizar el modelo:", error);
   });
-});
